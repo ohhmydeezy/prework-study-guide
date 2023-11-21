80d95ea1fb4d9ed8cb957a99564c4413824184e1
OhhMyDeezy <ohhmydeezy@MacDeezy.ultrahub>
2023-11-21 17:23:37 +0000

    added randomiser to the topic selection in script

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
 var topics = ['HTML', 'CSS', 'Git', 'Javascript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(var x = 0; x < topics.length; x++){
-    console.log(topics[x]);
+function listTopics() {
+    for(var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 }
 
-/*
-if(topic === 'HTML') {
+function selectTopic() {
+    if(randomTopic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+    } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+    } else if (randomTopic === 'Git') {
     console.log("Let's study Git!");
-} else if (topic === 'Javascript') {
+    } else if (randomTopic === 'Javascript') {
     console.log("Let's study Javascript!");
-} else {
+    } else {
     console.log('Please try again!')
+    }
 }
-*/
+console.log('Here are the topics we learned through prework:');
+listTopics()
+console.log('which topic should we study first?');
+selectTopic()
